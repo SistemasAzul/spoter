2b6f8cb4245123c146a9b1c681bbcbdfc3178168
SistemasAzul <sistemasazulcongelados@gmail.com>
2025-04-15 08:27:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'HEAD'])
 def enviar_pdf():
-    return send_file('foda.pdf', mimetype='application/pdf')
+    return send_file('lista_precio_1.pdf', mimetype='application/pdf')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Render le pasa un puerto
