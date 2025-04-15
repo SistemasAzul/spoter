639468bc161c311d1db9c169b4f602f5d225a1f8
SistemasAzul <sistemasazulcongelados@gmail.com>
2025-04-15 07:43:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, send_file
 
 app = Flask(__name__)
 
-@app.route('/C:/Users/Usuario/Downloads/')
+@app.route('foda')
 def enviar_pdf():
-    return send_file('20250414102815planilladecarga.pdf', mimetype='application/pdf')
+    return send_file('foda.pdf', mimetype='application/pdf')
 
 if __name__ == '__main__':
     app.run()
